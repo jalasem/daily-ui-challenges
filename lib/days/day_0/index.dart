63bdb8d0d6e698a38e8c5d557e64a8cc8aadbf4d
Abdulsamii Ajala <kgasta@gmail.com>
2024-06-01 10:36:19 +0100

    feat: day0

---
+++ lib/days/day_0/index.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Day0Counter extends StatefulWidget {
+  const Day0Counter({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Day0Counter> createState() => Day0CounterState();
+}
+
+class Day0CounterState extends State<Day0Counter> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
